53cfab8cbc510b0ef7a805392d0fbdd8516b0656
thealico <thealico@gmail.com>
2017-08-23 00:00:32 +0300

    kose

--- index.php
+++ index.php
@@ -1,8 +1,153 @@
+
+<!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Hey</title>
+		<title>Köşe Yazısı Dinle</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="common.css" />
+		<script src="common.js"></script>
 	</head>
+	
 	<body>
-		<h1><?php echo 'Merhaba Dünya' ?></h1>
+		
+		
+		<!--header-->
+		<header>
+			<a href="#"><img src="asset/koseyazisidinle.png" alt="Köşe Yazısı Dinle" title="Başlangıca dön"/></a>
+			<div>
+				<h3>topic tags</h3>
+				<p>Memphis area residents were warned on Saturday that the Mississippi River was gradually starting to "wrap its arms" around the city and rise to record levels as flooding moves south.</p>
+			</div>
+		</header>
+		
+		<!--authors-->
+		<section class="authors">
+			
+			<div>
+				<a class="prev"></a>
+				
+				<div>
+						
+					<ul>
+						<li><a href=""><img src="file/author/A1.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A2.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A3.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A4.jpg" alt="Author Name" /></a></li>
+						<li class="active_"><a href=""><img src="file/author/A11.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A6.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A7.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A8.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A12.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A10.jpg" alt="Author Name" /></a></li>
+					</ul>
+					<ul>
+						<li><a href=""><img src="file/author/A11.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A12.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A13.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A14.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A15.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A11.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A6.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A7.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A8.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A6.jpg" alt="Author Name" /></a></li>
+					</ul>
+					<ul>
+						<li><a href=""><img src="file/author/A7.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A1.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A2.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A2.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A3.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A4.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A11.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A6.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A7.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A8.jpg" alt="Author Name" /></a></li>
+					</ul>
+					<ul>
+						<li><a href=""><img src="file/author/A12.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A10.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A11.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A12.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A13.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A14.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A15.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A11.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A6.jpg" alt="Author Name" /></a></li>
+						<li><a href=""><img src="file/author/A6.jpg" alt="Author Name" /></a></li>
+					</ul>
+				
+				</div>
+				
+				<a class="next"></a>
+			</div>
+	
+		</section>
+		
+		<!--articel-->
+		<section class="articel">
+			<div>
+				
+				<aside>
+					<h5>Other Articels</h5>
+					<div>
+						<ul>
+							<li><a href=""> Wall Street hits resistance; LinkedIn soars in debut</a></li>
+							<li><a href="">Oil slumps on weak jobs data, IEA output call</a></li>
+							<li><a href="">Gold, silver fall on U.S. data; QE2 eyed</a></li>
+							<li><a href="">Dollar drops after weak data; euro still vulnerable</a></li>
+							<li><a href=""> Wall Street hits resistance; LinkedIn soars in debut</a></li>
+							<li><a href="">Oil slumps on weak jobs data, IEA output call</a></li>
+							<li><a href="">Gold, silver fall on U.S. data; QE2 eyed</a></li>
+							<li><a href="">Dollar drops after weak data; euro still vulnerable</a></li>
+							<li><a href=""> Wall Street hits resistance; LinkedIn soars in debut</a></li>
+							<li><a href="">Oil slumps on weak jobs data, IEA output call</a></li>
+							<li><a href="">Gold, silver fall on U.S. data; QE2 eyed</a></li>
+							<li><a href="">Dollar drops after weak data; euro still vulnerable</a></li>
+							<li><a href=""> Wall Street hits resistance; LinkedIn soars in debut</a></li>
+							<li><a href="">Oil slumps on weak jobs data, IEA output call</a></li>
+							<li><a href="">Gold, silver fall on U.S. data; QE2 eyed</a></li>
+							<li><a href="">Dollar drops after weak data; euro still vulnerable</a></li>
+						</ul>
+					</div>
+				</aside>
+					
+				<div class="content">
+					<div class="cart">
+						<img src="file/author/A11.jpg" alt="Author Name" />
+						<h1>Obama shifts on Mideast deal</h1>
+						<p>Author By <a href="#">Jon Canner</a></p>
+					</div>
+					
+					<div id="player"></div>
+					
+					
+	
+					
+				</div>
+				
+			</div>
+		</section>
+		
+		<!--footer-->
+		<footer>
+			
+		
+			
+			<p>© Copyright 2011 Kose Yazısı Dinle, Tüm Hakları Saklı</p>
+			<ul>
+				<li><a href="">Who is ?</a></li>
+				<li><a href="">About</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+			
+		</footer>
+		
+		
+		<script type="text/javascript">  
+			AudioPlayer.embed("player", { soundFile:"http://x.co/XK2V" });  
+		</script>
+		
 	</body>
+	
 </html>
